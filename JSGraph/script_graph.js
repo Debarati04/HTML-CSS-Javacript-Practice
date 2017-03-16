eae5e6d57713fb2d1ba4254910d80862ad4efe3b
Debarati Sarkar <debarati.sarkar04@gmail.com>
2017-03-16 18:24:55 +0000

    Bar Graph using table

---
+++ JSGraph/script_graph.js
@@ -0,0 +1,17 @@
+function handler(){
+    var colorArray = ['red', 'blue', 'green', 'yellow', 'grey', 'pink'];
+    
+    var tdLen = document.getElementsByClassName('row')
+    for (var i=0; i<tdLen.length; i++){
+        var x = document.getElementsByTagName("input")[i].value;
+        var div = document.createElement('div');
+        div.style.backgroundColor = colorArray[i];
+        div.style.height = x*2 + 'px';
+        div.style.verticalAlign = 'bottom';
+        tdLen[i].innerHTML = "";
+        tdLen[i].appendChild(div);
+    }
+}
+
+document.getElementsByTagName("button")[0].addEventListener('click', handler);
+
